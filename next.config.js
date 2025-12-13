022d22bb388f89c6525c608301686223ce60528b
hammadsandhuu <hammadsandhuu@gmail.com>
2025-12-13 22:06:18 +0500

    Add Webpack alias configuration for improved module resolution

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-
+const path = require("path");
 
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
